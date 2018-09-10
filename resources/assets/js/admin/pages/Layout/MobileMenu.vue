3ea963d211d1e6462f24ebf2ad739e1e37d7b5c6
Antoine Dewaele <antoinee.dewaele@gmail.com>
2018-09-10 17:45:19 +0200

    last updates before demo

--- resources/assets/js/admin/pages/Layout/MobileMenu.vue
+++ resources/assets/js/admin/pages/Layout/MobileMenu.vue
@@ -1,4 +1,10 @@
 <template>
+  <ul class="nav nav-mobile-menu">
+    <li><a :href="'/sellers/' + this.$root.currentUser.organization.id" class="dropdown-toggle" data-toggle="dropdown">
+      <i class="material-icons">home</i>
+      <p>Access to my shop</p></a>
+    </li>
+  </ul>
 </template>
 <script>
 export default {
