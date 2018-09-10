2fdf371bff788723436d28722f6ed5cb5e006252
Antoine Dewaele <antoinee.dewaele@gmail.com>
2018-09-10 17:00:23 +0200

    add sentence on give opinion

--- resources/assets/js/admin/components/Tables/ReviewTable.vue
+++ resources/assets/js/admin/components/Tables/ReviewTable.vue
@@ -10,7 +10,7 @@
                     <md-icon v-if="item.review_state.name === 'ACCEPTED'">done</md-icon>
                     <md-icon v-if="item.review_state.name === 'CERTIFIED'">done</md-icon>
                     <md-icon v-if="item.review_state.name === 'CREATED'">hourglass_empty</md-icon>
-                    <md-icon v-if="item.review_state.name === 'CLEAR'">clear</md-icon>
+                    <md-icon v-if="item.review_state.name === 'REFUSED'">clear</md-icon>
                 </md-table-cell>
                 <md-table-cell md-label="Created_at">{{ item.created_at }}</md-table-cell>
                 <md-table-cell>
